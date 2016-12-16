37c57d300a957915a2f4069db30b8572a7dfef92
Moritz Angermann <moritz.angermann@gmail.com>
2016-12-16 15:30:52 +0800

    Adds customized cabal2nix

--- default.nix
+++ default.nix
@@ -2,7 +2,11 @@
 
 (nixpkgs.pkgs.haskell.packages.${compiler}.override {
   packageSetConfig = nixpkgs.pkgs.callPackage ./lts-7.13-configuration.nix { };
+  overrides = self: super: {
+    cabal2nix = self.callPackage ./cabal2nix/cabal2nix.nix {};
+  };
 }).callPackage ./stackage2nix.nix { }
 
 # nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./stackage2nix.nix { }
+
 
